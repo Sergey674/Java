cf68f7c4c141fec30087a1746d55e04b808a3b71
Sergey674 <50282050+Sergey674@users.noreply.github.com>
2020-05-16 04:30:29 +0300

    Add files via upload

--- DownloadImages/src/DownloadImage/URLDownload.java
+++ DownloadImages/src/DownloadImage/URLDownload.java
@@ -9,8 +9,8 @@ public class URLDownload {
     // Сохраняет картинку, размером больше min КБ, по указанному url, в файл SavePath
     public static Runnable downloadFile(String url, String SavePath, int min) {
 
-        try
-        {
+        try {
+
             HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
 
             conn.setRequestMethod("GET");
@@ -21,27 +21,27 @@ public class URLDownload {
                 return null;
             }
 
-            InputStream in = new DataInputStream(conn.getInputStream());
-            OutputStream out = new FileOutputStream(SavePath);
+            //System.out.println('\n' + SavePath + "  !!!  " + url);
+            InputStream in = new BufferedInputStream(conn.getInputStream());
+            BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(SavePath));
 
             byte buff[] = new byte[1];
-            //int  num = in.read(buff);
 
             while (in.read(buff) > 0) {
                 out.write(buff, 0, 1);
+                //in.read(buff);
             }
 
 
-            System.out.println("Картинка с адресом:  " + '\n' + url + '\n' +  " сохранена в " + SavePath + '\n');
+            System.out.println("Картинка с адресом:  " + '\n' + url + '\n' + " сохранена в " + SavePath + '\n');
             Main.numImage.getAndIncrement();
 
             in.close();
             out.flush();
             out.close();
-        } catch (FileNotFoundException e){
-
-        }
-        catch (Exception e) {
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
@@ -50,16 +50,17 @@ public class URLDownload {
     public static Runnable download(String url, String PathImages, String SavePath, int min) {
         String nameImage = "";
 
-        if(!PathImages.matches(".*.(?:jpg|svg|gif|png)$")){
-            nameImage = String.valueOf((long) (Math.random() * 100000000000L)) + ".png";
-        }
-
-        if (PathImages.matches("\\/*")) {
-            URLDownload.downloadFile(url + PathImages, SavePath + nameImage, min);
-        } else if (PathImages.startsWith("//")) {
-            URLDownload.downloadFile("https:" + PathImages, SavePath + nameImage, min);
-        } else if (PathImages.startsWith("http")) {
-            URLDownload.downloadFile(PathImages, SavePath + nameImage, min);
+        if (!PathImages.matches(".*.(?:jpg|svg|gif|png)$")) {
+            nameImage = String.valueOf ((int) (Math.random() * 100000000)) + String.valueOf((int) (Math.random() * 100000000)) + ".png";
+            if (PathImages.startsWith("/")) {
+                URLDownload.downloadFile(url + PathImages, SavePath + nameImage, min);
+                if (PathImages.startsWith("//")) {
+                    URLDownload.downloadFile("https:" + PathImages, SavePath + nameImage, min);
+                    if (PathImages.startsWith("http")) {
+                        URLDownload.downloadFile(PathImages, SavePath + nameImage, min);
+                    }
+                }
+            }
         }
         return null;
     }
